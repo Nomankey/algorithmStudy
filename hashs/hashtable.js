7c49249d2d04603f2f1efa7ebe7f2736b7cb8e4b
Shawn CK.P <snowy213@outlook.com>
2021-08-20 11:20:06 -0700

    implemented hashtable

---
+++ hashs/hashtable.js
@@ -0,0 +1,61 @@
+function hashStringToInt(string, tableSize) {
+    let hash = 17;
+  
+    for (let i = 0; i < string.length; i++) {
+      hash = 13 * string.charCodeAt(i) % tableSize;
+    }
+  
+    return hash;
+  }
+
+class HashTable{
+
+    table = new Array(3333);
+    numItems = 0;
+
+    resize = () => {
+        const newTable = new Array(this.table.length * 2);
+        this.table.forEach(item => {
+            if(item) {
+                item.forEach(([key, value]) => {
+                    const index = hashStringToInt(key, newTable.length);
+                    newTable[index]? newTable[index].push([key, value]) : newTable[index] = [[key, value]];
+                });
+            }
+        });
+        this.table = newTable;
+    };
+
+    setItem = (key, value) => {
+        this.numItems++;
+        const loadTracker = this.numItems / this.table.length;
+        if (loadTracker > .8) {
+            this.resize();
+        }
+        const index = hashStringToInt(key, this.table.length);
+        this.table[index]? this.table[index].push([key, value]) : this.table[index] = [[key, value]];
+    };
+    getItem = key => {
+        const index = hashStringToInt(key, this.table.length);
+        return !this.table[index]? null : this.table[index].find(x => x[0] === key)[1];
+    }; 
+}
+
+const myTable = new HashTable();
+
+myTable.setItem("firstName", "bob");
+myTable.setItem("lastName", "park");
+myTable.setItem("age", "46");
+myTable.setItem("dateOfBirth", "25/AUG/1975")
+
+myTable.getItem("firstName");
+myTable.getItem("lastName");
+myTable.getItem("age");
+myTable.getItem("dateOfBirth");
+
+console.log(myTable.table.length)
+console.log(myTable.table)
+console.log(myTable.getItem("firstName"));
+console.log(myTable.getItem("lastName"));
+console.log(myTable.getItem("age"));
+console.log(myTable.getItem("dateOfBirth"));
