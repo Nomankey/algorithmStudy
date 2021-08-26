a9738d9f24b6e7c84fd48c495d76ec8932d069f4
Shawn CK.P <snowy213@outlook.com>
2021-08-25 19:22:17 -0700

    hash1

--- hashs/hash1.js
+++ hashs/hash1.js
@@ -1,11 +1,13 @@
-const participant = ["mislav", "stanko", "mislav", "ana"];
-const completion = 	["stanko", "ana", "mislav"];
+const participants = ["mislav", "stanko", "mislav", "ana"];
+const completions = 	["stanko", "ana", "mislav"];
 
 participant.sort();
 completion.sort();
 
-for(let i = 0; i < participant.length; i++) {
-    if(participant[i] !== completion[i]) {
-        return participant[i]
+function solution(participant, completion) {
+    participant.sort();
+    completion.sort();
+    for(let i = 0; i < participant.length; i++){
+        if(participant[i] !== completion[i]) return participant[i];
     }
 }
